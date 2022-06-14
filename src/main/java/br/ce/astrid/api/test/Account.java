00a14cfea914d41d6a8f627b13c2b06f6ca7ab54
Allan Scala <49344838+AllanScala1991@users.noreply.github.com>
2022-06-14 15:56:22 -0300

    feat: create user account api tests

--- src/main/java/br/ce/astrid/api/test/Account.java
+++ src/main/java/br/ce/astrid/api/test/Account.java
@@ -1,20 +1,157 @@
 package br.ce.astrid.api.test;
-import static io. restassured. RestAssured.given;
-import static org.hamcrest.Matchers.*;
-import org.junit.Test;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import com.github.javafaker.Faker;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+
+import br.ce.astrid.api.utils.Utils;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Account {
 	
+	private static String fakeName;
+	private static String fakeEmail;
+	private static String fakePassword;
+	private static Faker faker;
+
+	@BeforeClass
+	public static void setup()throws IOException{
+		baseURI = "http://localhost";
+		port = 8000;
+		faker = new Faker();
+		fakeName = faker.name().firstName();
+		fakeEmail = fakeName + "@mail.com";
+		fakePassword = faker.number().digits(5);
+	}
+
 	@Test
-	public void createNewAccount() {
+	public void t01_createNewAccount() {
+		Map<String, String> newUser = new HashMap<String, String>();
+		newUser.put("name", fakeName);
+		newUser.put("email", fakeEmail);
+		newUser.put("password", fakePassword);
+		
 		given()
-			.log().all()
+			.contentType("application/json")
+			.body(newUser)
 		.when()
-			.get("https://viacep.com.br/ws/83701300/json/")
+			.post("/user/create")
 		.then()
-			.log().all()
+			.body("status", is(true))
+			.body("message", is("Usuário criado com sucesso."))
 		;
 		
 	}
 	
+	@Test
+	public void t02_sendInvalidBody() {
+		Map<String, String> newUser = new HashMap<String, String>();
+		newUser.put("name", fakeName);
+		newUser.put("email", fakeEmail);
+		
+		given()
+			.contentType("application/json")
+			.body(newUser)
+		.when()
+			.post("/user/create")
+		.then()
+			.body("status", is(false))
+			.body("message", is("Todos os campos devem ser preenchidos."))
+		;
+	}
+
+	@Test
+	public void t03_getOneUserByID() throws IOException {
+		Object userID =  Utils.getUserID(fakeEmail, fakePassword);
+		given()
+		.when()
+			.get("/user/find/"+ userID)
+		.then()
+			.body("status", is(true))
+			.body("data.id", is(userID))
+			.body("data.name", is(fakeName))
+			.body("data.email", is(fakeEmail))
+			.body("data.password", is(notNullValue()))
+		;
+	}
+	
+	@Test
+	public void t04_sendInvalidUserID() {
+		given()
+		.when()
+			.get("/user/find/invalidID")
+		.then()
+			.body("data", nullValue())
+		;
+	}
+
+	@Test
+	public void t05_updateUser() throws IOException{
+		String userID =  Utils.getUserID(fakeEmail, fakePassword).toString();
+		Map<String, String> user = new HashMap<String,String>();
+		user.put("name", "update Name");
+		user.put("email", "updateMail@mail.com");
+		user.put("id", userID);
+
+		given()
+			.contentType("application/json")
+			.body(user)
+		.when()
+			.put("/user/update")
+		.then()
+			.body("status", is(true))
+			.body("message", is("Usuário atualizado com sucesso."))
+		;
+	}
+
+	@Test
+	public void t06_sendInvalidUserID() {
+		Map<String, String> user = new HashMap<String,String>();
+		user.put("name", "update Name");
+		user.put("email", "updateMail@mail.com");
+		user.put("id", "invalid");
+
+		given()
+			.contentType("application/json")
+			.body(user)
+		.when()
+			.put("/user/update")
+		.then()
+			.body("status", is(false))
+			.body("message.code", is("P2025"))
+			.body("message.meta.cause", is("Record to update not found."))
+		;
+	}
+
+	@Test
+	public void t07_deleteUserWithInvalidID() {
+		given()
+		.when()
+			.delete("/user/delete/invalidID")
+		.then()
+			.body("status", is(false))
+			.body("message.meta.cause", is("Record to delete does not exist."))
+		;
+	}
+
+	@Test
+	public void t08_deleteUser() throws IOException {
+		String userID =  Utils.getUserID("updateMail@mail.com", fakePassword).toString();
+
+		given()
+		.when()
+			.delete("/user/delete/"+userID)
+		.then()
+			.body("status", is(true))
+			.body("message", is("Usuário deletado com sucesso."))
+		;
+	}
 }
+
